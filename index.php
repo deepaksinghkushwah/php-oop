c706c335f4aef4fee34091a1b7d2aa27154b0d91
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2023-03-14 14:21:50 +0530

    efasf

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ spl_autoload_register(function($className){
 $conn = mysqli_connect('localhost','root','','phpoop');
 // initlize class object
 $obj = new Student;
+//$obj->createStudent(); 1
+//Student::createStudent2(); 2
 $obj->name = "Deeaak";
 $obj->age = 50;
 $obj->standard = 18;
